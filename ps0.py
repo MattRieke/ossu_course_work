f35f0575ec02c914b71152b8f6f40d6843f263cb
MattRieke <75626231+MattRieke@users.noreply.github.com>
2021-01-24 16:07:32 -0800

    problem set 0 upload

---
+++ ps0.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 10 12:23:03 2021
+
+@author: mattr
+"""
+
+import numpy
+
+x = float(input('Enter number x: '))
+y = float(input('Enter number y: '))
+
+print('x**y = ' + str(x**y))
+print('log(x) = ' + str(numpy.log2(x)))
+
+
---
+++ pyTest.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 10 12:19:45 2021
+
+@author: mattr
+"""
+
+print('hello world')
+print('I like 6.001!')
